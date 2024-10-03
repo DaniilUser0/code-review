1db8bb065f17923e4e0f0aabe7c02cb69ba588d0
Artem <bulanov20024@yandex.ru>
2024-10-03 17:34:47 +0400

    task11

--- task11.js
+++ task11.js
@@ -1,12 +1,21 @@
-function checkAge(age)
+/*function checkAge(age)
 {
     age = '18';
     if(age < 18) console.log("you are not allowed");
     if(age >= 18) console.log("you are welcome!");
 }
 checkAge();
+*/
+
+function checkAge(age) 
+{
+    if(age < 18) console.log("you are not allowed");
+    if(age >= 18) console.log("you are welcome!");
+}
+checkAge('23');
 
 //Калькулятор
+
 function calc(operation, a, b)
 {
     if(operation === 'add') console.log(a + b);
