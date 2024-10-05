14f6fc5cb4f631bb2016fea78bca63029537e6b7
Ilya Tarasov <tarasovilya2000@gmail.com>
2024-10-05 21:20:23 +0300

    написал тебе через if

---
+++ main.js
@@ -0,0 +1,10 @@
+const pogoda = 23;
+if (pogoda>=0 && 10>=pogoda) {
+    console.log("0-10");
+}
+if (pogoda>=10 && 20>=pogoda) {
+    console.log("10-20")
+}
+if (pogoda>=20 && 30>=pogoda) {
+    console.log("20-30")
+}
