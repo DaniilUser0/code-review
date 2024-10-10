df207fcaab11c3c5c98f416fc0ee61b442cf22ae
Ilya Tarasov <tarasovilya2000@gmail.com>
2024-10-10 22:31:21 +0300

    проверка

--- test_globale.js
+++ test_globale.js
@@ -2,7 +2,7 @@ const phoneBook = {
     list: { // контактам дали свойство лист
     "Daniil Brat": 23, // ключ всегда цифры
     "Artem brat": 22, // ключ - возраст
-    "Shuklin Daniil": 24
+    "Shuklin Daniil": 24 //
   },
 
 
