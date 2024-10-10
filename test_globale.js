da2158262c01be8d748b4ed3c5c5e45e0242cfc8
Ilya Tarasov <tarasovilya2000@gmail.com>
2024-10-10 22:16:01 +0300

    домашка за 8.10.24

---
+++ test_globale.js
@@ -0,0 +1,15 @@
+const phoneBook = {
+    list: { // контактам дали свойство лист
+    "Daniil Brat": 23, // ключ всегда цифры
+    "Artem brat": 22, // ключ - возраст
+    "Shuklin Daniil": 24
+  },
+
+
+  log() { 
+    console.log(this.list)
+    
+  }
+}
+  
+  phoneBook.log();
